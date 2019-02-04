3d457c252d21698bb77a568f4e506222c693d901
Piotr Michna <poczta@piotrmichna.pl>
2019-02-04 14:12:25 +0100

    definicja procedury wysylania w funkdji srSend

--- pmSR/pmSR.c
+++ pmSR/pmSR.c
@@ -18,7 +18,27 @@ void srInit(TSR *sr){
 }
 
 void srSend(TSR *sr){
-	if(sr->sr_typ){
+	if(sr->sr_typ && sr->ser.PORTX && sr->sck.PORTX && sr->rck.PORTX){
+		uint8_t *reg;
+		uint8_t pin;
 
+		reg=sr->pin_buf;
+		reg+=sr->num-1;
+		for(uint8_t i=0;i<sr->num;i++){
+			if(sr->dir) pin=0x80; else pin=0x01;
+			while(pin){
+				if( (*reg & pin) ){
+					if(sr->on_bit) setIO(&sr->ser); else resIO(&sr->ser);
+				}else{
+					if(sr->on_bit) resIO(&sr->ser); else setIO(&sr->ser);
+				}
+				setIO(&sr->sck);
+				resIO(&sr->sck);
+				if(sr->dir) pin>>=1; else pin<<=1;
+			}
+			reg--;
+		}
+		setIO(&sr->rck);
+		resIO(&sr->rck);
 	}
 }
