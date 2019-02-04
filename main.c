db5d962dd7fb9b17c93e06470f103d4b5904f13f
Piotr Michna <poczta@piotrmichna.pl>
2019-02-04 21:05:56 +0100

    test sroSetBit(n,1)

--- main.c
+++ main.c
@@ -10,10 +10,23 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include "pmSRO/pmSRO.h"
+
+
 
 int main(void){
+	uint8_t cnt=0,n=0;
 
 	while(1){
+		sroEvent();
+		if(cnt==99){
+			sroSetBit(n,1);
+			n++;
+			if(n>7) n=0;
+
+		}else{
+			cnt++;
+		}
 
 		_delay_ms(10);
 	}
