fa3b99f833f2c7ef32f77a2c222e85bd7b73a8ad
Piotr Michna <poczta@piotrmichna.pl>
2019-02-04 12:30:38 +0100

    dodanie pliku glownego projektu

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: 4 lut 2019
+ *      Author: Piotr Michna
+ *		e-mail: poczta@piotrmichna.pl
+ */
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void){
+
+	while(1){
+
+		_delay_ms(10);
+	}
+}
