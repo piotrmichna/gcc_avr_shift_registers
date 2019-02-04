aab6c40ce2258fa30fa5618aba38aed1245a5504
Piotr Michna <poczta@piotrmichna.pl>
2019-02-04 13:09:21 +0100

    definicja typu strukturalnego TSR

--- pmSR/pmSR.h
+++ pmSR/pmSR.h
@@ -14,4 +14,25 @@ typedef struct{
 	uint8_t MASK;
 }TIO;
 
+typedef struct{
+	TIO ser;
+	TIO sck;
+	TIO rck;
+#ifdef OSR0_EN_PIN
+	TIO en;
+#endif
+#ifdef OSR0_PWR_PIN
+	TIO pwr;
+#endif
+#ifdef OSR0_LED_PIN
+	TIO led;
+#endif
+	uint8_t *pin_buf;
+	uint8_t num;	// ilosc rejestrow
+	uint8_t sr_typ;	// 1=rejestr wyjsciowy 0=rejestr wejsciowy
+	uint8_t dir;	// kierunek wsuwania bitow
+	uint8_t on_bit;	// bit wlaczany 0 lub 1
+	uint8_t enable;	// 0=wylaczony 1= aktywny
+}TSR;
+
 #endif /* PMSR_PMSR_H_ */
