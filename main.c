9e01ec7ff9f049485f0590fa574d3821d65eccb9
Piotr Michna <poczta@piotrmichna.pl>
2019-02-05 18:30:32 +0100

    poprawki po testach main.c

--- main.c
+++ main.c
@@ -12,6 +12,8 @@
 
 #include "pmSRO/pmSRO.h"
 #include "pmSRI/pmSRI.h"
+#include <avr/interrupt.h>
+#include "pmUART/pmuart.h"
 
 //uint8_t sri_buf[ISR0_REG_NUM];
 uint8_t flag=0;
@@ -21,6 +23,10 @@ void setPk(uint8_t id, uint8_t val);
 int main(void){
 
 	_delay_ms(3000);
+	USART_Init( __UBRR);
+
+	sei();
+	uart_puts("start\r\n");
 	sriRegisterUpdate(setPk);
 
 	while(1){
@@ -29,12 +35,18 @@ int main(void){
 
 		if(flag) sroSend();
 		flag=0;
-		_delay_ms(10);
+		_delay_ms(500);
 	}
 }
 
 
 void setPk(uint8_t id, uint8_t val){
 	flag=1;
+	uart_puts("setPk");
+//	uart_putint(id,10);
+//	uart_puts(", ");
+//	uart_putint(val, 10);
+	uart_puts("\r\n");
+
 	sroSetBit(id, val);
 }
