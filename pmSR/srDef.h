cb46de0c0b3f204201eda42ce34ee7c6718de7a6
Piotr Michna <poczta@piotrmichna.pl>
2019-02-05 12:47:46 +0100

    zmiana wartosci makrodefinicji SRO_AUTO_SEND = 0

--- pmSR/srDef.h
+++ pmSR/srDef.h
@@ -12,7 +12,7 @@
 #define SR_EN 1		// dostepne sterowanie linia CE rejestru
 #define SR_PWR 1	// dostepne sterowanie linia zasilania rejestru
 #define SR_LED 1	// dostepna kotrolka pracy rejestru
-#define SRO_AUTO_SEND 1
+#define SRO_AUTO_SEND 0
 
 #ifdef SR_PWR
 #define SR_PWR_ON_BIT	1	// zakomentuj jesli sterwanie pinem zgodny z ustawianiem stanu pinu rejestru
